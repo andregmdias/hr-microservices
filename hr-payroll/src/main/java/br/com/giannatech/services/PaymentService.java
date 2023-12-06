7eef5c96b2329d98f123682119536f6f1f2c405c
André Luiz Giannattasio Marques Dias <andregmdias@gmail.com>
2023-12-05 22:52:26 -0300

    feat: implementa RestTemplate

--- hr-payroll/src/main/java/br/com/giannatech/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/giannatech/services/PaymentService.java
@@ -1,13 +1,33 @@
 package br.com.giannatech.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.giannatech.entities.Payment;
+import br.com.giannatech.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String hrWorkerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		
+		uriVariables.put("id", Long.toString(workerId));
+		
+		Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}" , Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
