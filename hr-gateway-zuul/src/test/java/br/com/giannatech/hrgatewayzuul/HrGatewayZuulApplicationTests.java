ec7edad33141b951b66813762ba2a93ad6feee30
André Luiz Giannattasio Marques Dias <andregmdias@gmail.com>
2023-12-12 20:19:39 -0300

    project hr-api-gateway-zuul created

---
+++ hr-gateway-zuul/src/test/java/br/com/giannatech/hrgatewayzuul/HrGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giannatech.hrgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
