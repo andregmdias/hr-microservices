472cf28dd3bf71aa6fe1f467bb7e9281522c9072
andregmdias <andregmdias@gmail.com>
2024-01-16 15:22:07 -0300

    feat: configuring cors

--- hr-oauth/src/main/java/br/com/giannatech/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/br/com/giannatech/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package br.com.giannatech.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,18 +10,21 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("myappsecret123");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
-	
+
 	@Bean
 	public JwtTokenStore jwtTokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
